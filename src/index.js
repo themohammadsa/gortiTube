d3b49fd297b3b269e1eefbd23d750db38d2a5214
Mohammad S <59393936+themohammadsa@users.noreply.github.com>
2021-05-05 03:36:45 +0530

    added: scrollContext

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { LibraryProvider } from "./Context/LibraryContext";
 import App from "./App";
 import { ToggleProvider } from "./Context/ToggleContext";
+import { ScrollProvider } from "./Context/ScrollContext";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <Router>
-      <LibraryProvider>
-        <ToggleProvider>
-          <App />{" "}
-        </ToggleProvider>
-      </LibraryProvider>
-    </Router>
+      {" "}
+      <ScrollProvider>
+        <LibraryProvider>
+          <ToggleProvider>
+            <App />{" "}
+          </ToggleProvider>
+        </LibraryProvider>{" "}
+      </ScrollProvider>
+    </Router>{" "}
   </StrictMode>,
   rootElement
 );
